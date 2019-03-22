386a74c94c345e3f76b84055aef054b078ab8a58
AnisO <aouared@cpaglobal.com>
2019-03-22 18:54:06 +0100

    [FROM_WORK] : Finishing chapter Angular Controllers & Markup

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,9 +2,15 @@
 
 eventsApp.controller('EventController',
     function EventController($scope) {
+
+        $scope.snippet = '<span style="color:red">Hi there</span>';
+        $scope.boolValue = true;
+        $scope.mystyle = {color:'red'};
+        $scope.myclass= "blue";
+        $scope.sortorder = 'name';
         $scope.event = {
             name: 'Angular Boot Camp',
-            date: '1/1/2013',
+            date: 1359781015626,
             time: '10:30 am',
             location: {
                 address: 'Google Headquaters',
@@ -16,24 +22,24 @@ eventsApp.controller('EventController',
                 {
                     name: 'Directives Masterclass',
                     creatorName: 'Bob Smith',
-                    duration: '1 hr',
-                    level: 'Advanced',
+                    duration: 1,
+                    level: 3,
                     abstract: 'In this session you will learn ths ins and outs of ...',
                     upVoteCount: 0
                 },
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'John Doe',
-                    duration: '30 mins',
-                    level: 'Introductory',
+                    duration: 2,
+                    level: 1,
                     abstract: 'This session will take a closer loot at scopes, ...',
                     upVoteCount: 0
                 },
                 {
                     name: 'Well Behaved Controllers',
                     creatorName: 'Jane Doe',
-                    duration: '2 hours',
-                    level: 'Intermediate',
+                    duration: 4,
+                    level: 2,
                     abstract: 'Controllers are the beginning of everything Angular does ...',
                     upVoteCount: 0
                 }
--- app/js/filters.js
+++ app/js/filters.js
@@ -1,1 +1,29 @@
 'use strict';
+
+eventsApp.filter('durations', function() {
+    return function(duration) {
+        switch(duration) {
+            case 1:
+                return "Half Hour";
+            case 2:
+                return "1 Hour";
+            case 3:
+                return "Half Day";
+            case 4:
+                return "Full Day"
+        }
+    }
+});
+
+eventsApp.filter('levels', function () {
+    return function(level) {
+        switch(level) {
+            case 1:
+                return '/img/level1.png';
+            case 2:
+                return '/img/level2.png';
+            case 3:
+                return '/img/level3.png';
+        }
+    }
+});
